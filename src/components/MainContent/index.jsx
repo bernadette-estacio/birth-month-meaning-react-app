69d71aa4e2fdc6be42942befb786999d9f9e0044
Bernadette Estacio <bernadette@englemaninvest.com>
2020-12-24 04:49:16 -0500

    update syntax

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,16 +1,15 @@
-// import React, { useState, useRef, useEffect } from 'react';
 import React from 'react';
 import "./styles.css";
 
 const MainContent = ({ month, meaning, clickMonthState }) => {
-  const contentClass = clickMonthState.clickMonth ? "visible" : "invisible";
+  const clickMonth = clickMonthState.clickMonth;
 
   return (
     <div className="mainContent">
       <h1>Birth Month Meaning</h1>
       <hr className="hrStyle" />
       <h2>{month}</h2>
-      <div className={contentClass} id="content">{meaning}</div>
+      <div className={clickMonth ? "visible" : "invisible"}>{meaning}</div>
     </div>
   );
 };
